ac96192e07e326c5bafd92e21992eff38364f840
Kim-Mark <qsik1218000@gmail.com>
2024-08-07 22:18:07 +0900

    주석처리

--- src/main/java/com/erica/project/User/repository/EmployeeRepository.java
+++ src/main/java/com/erica/project/User/repository/EmployeeRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // Employee 엔티티에 특화된 쿼리 정의
+    // 내 이름은 김규식
 }
